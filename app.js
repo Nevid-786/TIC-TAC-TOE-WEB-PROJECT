6c2d0aab0444a741619cd2c63bc9fcfa7a0390f6
Nevid-786 <mdnabid786@gmail.com>
2025-10-28 13:41:28 +0530

    Updated the working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let boxes=document.querySelectorAll(".boxes");
 let resetbtn=document.querySelector("#reset");
 let turnO=true;//playeO
+let result=document.querySelector("#result");
 let patterns=[
     [0,1,2],
     [3,4,5],
@@ -10,16 +11,24 @@ let patterns=[
     [2,5,8],
     [0,4,8],
     [2,4,6],
-]
+];
+let disable= ()=>{
+    boxes.forEach((box)=>{
+        box.disabled=true;
+       
+    });
+}
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
     if(turnO){
         box.innerText="O";
+        result.innerText="Turn:O";
         turnO=false;
     }
     else{
         box.innerText="X";
         turnO=true;
+        result.innerText="Turn:X";
 
     }
     box.disabled=true;
@@ -32,9 +41,12 @@ const checkWinner=()=>{
     let pos1Value=boxes[pattern[0]].innerText;
     let pos2Value=boxes[pattern[1]].innerText;
     let pos3Value=boxes[pattern[2]].innerText;
+    
     if(pos1Value!="" && pos2Value!=""  && pos3Value!="" ){
     if(pos1Value===pos2Value && pos2Value===pos3Value){
         console.log(`Winner is ${pos1Value}`);
+        result.innerText=`Winner is ${pos1Value}`;
+        disable();
         
     }
     
@@ -71,6 +83,7 @@ resetbtn.addEventListener("click",()=>{
        
     });
      console.log("reset");
+     result.innerText="Start Playing";
 
 }
 )
