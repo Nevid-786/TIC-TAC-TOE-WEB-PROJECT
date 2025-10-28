779a1169e336497267446e377304312156aab26b
Nevid-786 <mdnabid786@gmail.com>
2025-10-28 18:43:12 +0530

    added winner panel and turn status with higlighting winning pattern

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let boxes=document.querySelectorAll(".boxes");
 let resetbtn=document.querySelector("#reset");
+let A=document.querySelector("#A");
 let turnO=true;//playeO
+
+
 let result=document.querySelector("#result");
 let patterns=[
     [0,1,2],
@@ -22,13 +25,13 @@ boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
     if(turnO){
         box.innerText="O";
-        result.innerText="Turn:O";
+        result.innerText="Turn:X";
         turnO=false;
     }
     else{
         box.innerText="X";
         turnO=true;
-        result.innerText="Turn:X";
+        result.innerText="Turn:O";
 
     }
     box.disabled=true;
@@ -41,10 +44,14 @@ const checkWinner=()=>{
     let pos1Value=boxes[pattern[0]].innerText;
     let pos2Value=boxes[pattern[1]].innerText;
     let pos3Value=boxes[pattern[2]].innerText;
+    console.log(pattern);
     
     if(pos1Value!="" && pos2Value!=""  && pos3Value!="" ){
     if(pos1Value===pos2Value && pos2Value===pos3Value){
         console.log(`Winner is ${pos1Value}`);
+        boxes[pattern[0]].style.backgroundColor="red";
+        boxes[pattern[1]].style.backgroundColor="red";
+        boxes[pattern[2]].style.backgroundColor="red";
         result.innerText=`Winner is ${pos1Value}`;
         disable();
         
@@ -55,30 +62,11 @@ const checkWinner=()=>{
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 resetbtn.addEventListener("click",()=>{
     boxes.forEach((box)=>{
         box.innerText=" ";
+        box.style.backgroundColor="#96BBBB";
+
         box.disabled=false;
        
     });
